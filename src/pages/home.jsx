d0061418b84b56417198ed5b382cf63701c611bf
Maryam Tufail <maryamtufail78@gmail.com>
2024-01-28 04:27:43 +0500

    banner responsive

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import Hero from "../containers/hero"
+import Hero from "../containers/hero";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+
 const Home = () => {
+  useDocumentTitle("Salinaka | Home");
   return (
     <div className="py-[3rem] px-[4rem] relative flex ">
       <Hero />
